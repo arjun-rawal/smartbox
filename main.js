36fe86354427a3dab0b0d67f330d8dd54979aaf4
arjun-rawal <107657794+arjun-rawal@users.noreply.github.com>
2022-12-17 15:21:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,25 +10,25 @@ localStorage.clear();
 
 
 }
-function newTask(){
-    let taskName = prompt("Task Name: ", "task")
-    displayTasks(taskName);
-}
+// function newTask(){
+//     let taskName = prompt("Task Name: ", "task")
+//     displayTasks(taskName);
+// }
 
-function displayTasks(taskName){
-    //creates a paragraph with the task name
+// function displayTasks(taskName){
+//     //creates a paragraph with the task name
     
-    const td = document.getElementById("tasks");
-    const para = document.createElement("p");
-    para.className="tasks"
-    const node = document.createTextNode(taskName);
-    para.appendChild(node); 
-    td.appendChild(para);
+//     const td = document.getElementById("tasks");
+//     const para = document.createElement("p");
+//     para.className="tasks"
+//     const node = document.createTextNode(taskName);
+//     para.appendChild(node); 
+//     td.appendChild(para);
 
-    //store task locally
-    localStorage.setItem(numOfTasks.toString(), taskName);
-    numOfTasks++;
-}
+//     //store task locally
+//     localStorage.setItem(numOfTasks.toString(), taskName);
+//     numOfTasks++;
+// }
 
 function Blue(){
 
